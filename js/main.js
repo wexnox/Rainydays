f4e1f6e275bd58e1cb030b41465804670a294af8
wexnox <lasseronnlie@hotmail.com>
2021-01-12 14:12:59 +0100

    Nav v0.1 + added files for link purp

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+function toggleMobile() {
+    var x = document.getElementById("mobile-menu");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
